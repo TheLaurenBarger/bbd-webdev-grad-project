6e777a4297928f877d5b185f836d74c6be782906
mengess <storm.menges@vcontractor.co.za>
2019-05-30 14:14:05 +0200

    halfway

---
+++ src/app/image-picker/image-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'image-picker',
+    templateUrl: 'image-picker.component.html'
+  })
+  export class ImagePickerComponent{
+    title = 'Profile Picture';
+    images = ["assets/images/male.jpg","assets/images/female.jpg","assets/images/others.jpg"];
+  }
+
