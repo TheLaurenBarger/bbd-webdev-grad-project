61be2240f2bb164451aa5f9a1b012df8bcaf8564
Darryn <darryn@bbd.co.za>
2019-05-30 18:42:04 +0200

    Added mockdb and backend calls

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const MockDB = require('./mockdb')
 
 app.use((req,res,next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,12 +10,29 @@ app.use((req,res,next) => {
 });
 
 app.use('/user', (req,res,next) => {
+  let user = MockDB.MockUserDB.find(i => i.id === req.query.id);
+  if (user === undefined) res.status(404)
+  else res.status(200).json(user);
+  return;
+});
 
-  res.status(200).json({
-    id: 'ewsrydtfgyuh',
-    name: 'Lauren Barger',
-    title: 'Master Breaker'
-  });
+
+app.use('/validate', (req,res,next) => {
+  let user = MockDB.MockUserDB.find(i => i.username === req.query.username);
+
+  res.status(401).json('Username or password incorrect')
+  if (user === undefined) return;
+  if (user.password == req.query.pass) res.status(200).json(user.id);
+  return;
+});
+
+app.use('/tracks', (req,res,next) => {
+  let user = MockDB.MockUserDB.find(i => i.id === req.query.id);
+  let tracks = MockDB.MockTrackDB.find(i => i.id === req.query.id);
+  
+  if (user === undefined) res.status(404).json('User not found')
+  else res.status(200).json(tracks);
+  return;
 });
 
 app.use('/warrior', (req,res,next) => {
