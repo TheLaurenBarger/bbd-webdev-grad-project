b6696e13ab9eba3028b8132c70947a54f0727cd5
Darryn <darryn@bbd.co.za>
2019-05-30 19:38:38 +0200

    Fixed

--- Backend/routes/validate.js
+++ Backend/routes/validate.js
@@ -6,10 +6,12 @@ const MockDB = require('../mockdb');
 
 router.get('', (req,res,next) => {
   let user = MockDB.MockUserDB.find(i => i.username === req.query.username);
-
-  res.status(401).json('Username or password incorrect')
-  if (user === undefined) return;
-  if (user.password == req.query.pass) res.status(200).json(user.id);
+  if (user === undefined)
+  {
+    res.status(401).json('Username or password incorrect');
+    return;
+  }
+  if (user.password == req.query.password) res.status(200).json(user.id);
   return;
 });
 
